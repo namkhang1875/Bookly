a8c18f3858f1600f95be73c487c3ef1e12e341bb
namkhang1875 <ice9966@hotmail.com>
2019-04-14 18:24:40 +0700

    ปรับปรุงระบบลงทะเบียน
    ปรับปรุงระบบลงทะเบียน เพิ่มการเช็ค email format เช็คการกรอกชื่อ รหัสผ่าน

--- js/app.js
+++ js/app.js
@@ -1,9 +1,54 @@
-function submitOnClick(){
-    var email=document.getElementById('email');
-    var dname=document.getElementById('dname');
-    var psw=document.getElementById('psw');
-    insertData(email.value,dname.value,psw.value);
+function signUpOnClick(){
+    var email=document.getElementById('email').value;
+    var dname=document.getElementById('dname').value;
+    var psw=document.getElementById('psw').value;
+    var cpsw = document.getElementById('cpsw').value;
+
+    var isEmailOk =false;
+    var isDnameOk =false;
+    var isPswOk = false;
+    var isCpswOk = false;
+
+  
+    var mailre = '^[0-9]{8}@kmitl.[a-z]{2}.[a-z]{2}$';
+    if(email.match(mailre)){
+        console.log("email Ok");
+        isEmailOk = true;
+    }
     
+    else if(!email.match(mailre)){
+        alert("โปรดใช้อีเมลสถาบัน");
+    }
+
+    if(dname.length==0){
+        alert("โปรดใส่ชื่อ");
+    }
+    else if(dname.length >0){
+        console.log("dname ok")
+        isDnameOk = true;
+    }
+
+    if(psw.length==0){
+        alert("โปรดใส่รหัสผ่าน");
+    }
+    else if(psw.length >0){
+        console.log("password Ok");
+        isPswOk = true;
+    }
+    if(cpsw.length==0){
+        alert("โปรดยืนยันรหัสผ่าน");
+    }
+
+    if(psw != cpsw){
+        alert("โปรดใส่รหัสผ่านให้ตรงกัน");
+    }
+    else if(psw == cpsw){
+        console.log("password match");
+        isCpswOk = true;
+    }
+    if((isEmailOk&&isDnameOk)&&(isPswOk&&isCpswOk)){
+        insertData(email,dname,psw);
+    } 
 }
 
 window.onload=function(){
@@ -24,5 +69,6 @@ function insertData(email,dname,psw){
         dname:dname,
         psw:psw
     });
-    console.log("ลงทะเบียนสำเร็จ");
+    alert("ลงทะเบียนสำเร็จ");
+    window.location.href="index.html";
 }
