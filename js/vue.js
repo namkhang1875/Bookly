0fd657920b41a91cef9afa985c81f62ed599a8ef
namkhang1875 <ice9966@hotmail.com>
2019-04-22 01:27:36 +0700

    เพิ่มส่วนของ post (ยังไม่สมบูรณ์)

--- js/vue.js
+++ js/vue.js
@@ -10,8 +10,19 @@ var book = new Vue({
         getBookName : function(){
             return this.name
         },
-        setName : function (name){
+        setBookName : function (name){
             return this.name = name
         }
     }
 })
+window.onload=function(){
+    
+    var firebaseRef = firebase.database().ref("Book").orderByChild("category").equalTo("การ์ตูน").on('value', function(dataSnapshot){
+            dataSnapshot.forEach(function(childSnapshot){
+            var key = childSnapshot.key;
+            book.setBookName(key);
+        });
+    });
+}
+var storage = firebase.storage();
+var gsReference = storage.refFromURL('gs://softdevdb-d19a4.appspot.com/1543556630014.jpg');
