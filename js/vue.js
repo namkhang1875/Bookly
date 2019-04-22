dfaaab84eec6a692f7cec1a32ace891f0301e283
namkhang1875 <ice9966@hotmail.com>
2019-04-22 21:54:16 +0700

    ดึงรูปจาก firebase storage ได้

--- js/vue.js
+++ js/vue.js
@@ -12,6 +12,20 @@ var book = new Vue({
         },
         setBookName : function (name){
             return this.name = name
+        },
+        getImage : function(){
+            var storage = firebase.storage();
+            var gsReference = storage.refFromURL('gs://softdevdb-d19a4.appspot.com/1543556630014.jpg');
+            storage.ref('1543556630014.jpg').getDownloadURL().then(function(url) {
+                // `url` is the download URL for 'images/stars.jpg'
+                // Or inserted into an <img> element:
+                var img = document.getElementById('myimg');
+                img.src = url;
+                console.log(url);
+                
+              }).catch(function(error) {
+                // Handle any errors
+              });
         }
     }
 })
@@ -21,8 +35,8 @@ window.onload=function(){
             dataSnapshot.forEach(function(childSnapshot){
             var key = childSnapshot.key;
             book.setBookName(key);
+            book.getImage();
         });
     });
 }
-var storage = firebase.storage();
-var gsReference = storage.refFromURL('gs://softdevdb-d19a4.appspot.com/1543556630014.jpg');
+
